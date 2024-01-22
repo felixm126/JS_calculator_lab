9f44c9e90f1a083667d2c51e7b407c1bf92e4268
Felix <felixwhman@gmail.com>
2024-01-22 15:48:24 -0500

    initial commit

---
+++ calculator/app.js
@@ -0,0 +1,10 @@
+/* WhiteBoard
+
+How can we store state of the calculator? How are we tracking?
+
+After a user click on a certain button, what code am i going to run?
+
+Try not to use a separate event handler for each button.
+    use for loop or forEach()
+
+*/
